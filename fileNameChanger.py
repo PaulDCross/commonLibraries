b66a06f62e777f0543589f8f871fcb3d5fe11b79
PaulDCross <paulcross@f2s.com>
2017-05-01 22:32:50 +0100

    removed filenamechanger

--- fileNameChanger.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def rename(dirname, commonFileName, newEnd):
-    for filename in os.listdir(dirname):
-        #enter the bit you want removed here
-        if filename.startswith(commonFileName):
-            #the 15 is the amount of character in the previous statment
-            os.rename(filename, commonFileName+newEnd)
